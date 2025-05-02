5413756c126289124321d1b9925f959885d8bdf5
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-05-02 22:35:13 +0530

    backend complete

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 // initialize dotenv
 dotenv.config()
 import connectDB from "./config/config.js"
+import cookieParser from "cookie-parser"
 
 // PORT
 const PORT = process.env.PORT || 5000
@@ -19,6 +20,9 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cookieParser())
+
 
 // user routes
 app.use("/api/users", userRoutes)
