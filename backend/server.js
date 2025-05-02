14d5305699abf483fe32a58b5ed44557af798623
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-05-02 22:07:17 +0530

    user auth

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,38 @@
 import express from "express"
-import dotenv from 'dotenv'
+import dotenv from "dotenv"
+// initialize dotenv
 dotenv.config()
+import connectDB from "./config/config.js"
+
 // PORT
 const PORT = process.env.PORT || 5000
 
 import userRoutes from "./routes/userRoutes.js"
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js"
 
+// connect mongodb
+connectDB()
 
+// initialize express
 const app = express()
 
-app.use('/api/users', userRoutes)
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+// user routes
+app.use("/api/users", userRoutes)
+
+// main page
 app.get("/", (req, res) => {
     res.send("Server is ready")
 })
 
+// middleware
+// error handlers
 app.use(notFound)
 app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`server running at port: ${PORT}`)
+    console.log(`server running at port: ${PORT}`) 
 })
