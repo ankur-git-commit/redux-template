fc7918bb5b2e76082725fe2c9e823631455a037a
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-05-03 14:55:30 +0530

    routing2

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,26 +1,22 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import {
-    createBrowserRouter,
-    createRoutesFromElements,
-    Route,
-    RouterProvider,
+    BrowserRouter,
+    Routes,
+    Route
 } from "react-router"
 import App from "./App.jsx"
 import HomeScreen from "./screens/HomeScreen.jsx"
 import "bootstrap/dist/css/bootstrap.min.css"
 import "./index.css"
-
-const router = createBrowserRouter(
-    createRoutesFromElements(
-        <Route path="/" element={<App />}>
-            <Route index={true} path="/" element={<HomeScreen />} />
-        </Route>
-    )
 )
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <RouterProvider router={router} />
+        <BrowserRouter>
+            <Routes>
+                <Route index element={}>
+            </Routes>
+        </BrowserRouter>
     </StrictMode>
 )
