3749fa520887029f276a74a5f8edbfc7c2f35bbf
ankur-git-commit <ankur.kumar.dev@outlook.com>
2025-05-04 11:53:59 +0530

    deployment configs

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express"
 import dotenv from "dotenv"
 // initialize dotenv
@@ -18,6 +19,7 @@ connectDB()
 const app = express()
 
 
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
@@ -27,10 +29,22 @@ app.use(cookieParser())
 // user routes
 app.use("/api/users", userRoutes)
 
-// main page
-app.get("/", (req, res) => {
-    res.send("Server is ready")
-})
+// Serve frontend in production
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    
+    // Set static folder
+    app.use(express.static(path.join(__dirname, '/frontend/dist')));
+    
+    // Any route that is not API will be redirected to index.html
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+  } else {
+    app.get('/', (req, res) => {
+      res.send('API is running....');
+    });
+  }
 
 // middleware
 // error handlers
